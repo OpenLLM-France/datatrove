51c49f5b5e7dda8fe39a2f4f003072787ff5b40f
Oligou <oliviergouvert@gmail.com>
2025-03-17 18:15:50 +0100

    pop only metadata if it is a dict

--- src/datatrove/pipeline/readers/base.py
+++ src/datatrove/pipeline/readers/base.py
@@ -58,12 +58,14 @@ class BaseReader(PipelineStep):
         Returns: a dictionary with text, id, media and metadata fields
 
         """
+
         return {
             "text": data.pop(self.text_key, ""),
             "id": data.pop(self.id_key, f"{path}/{id_in_file}"),
             "media": data.pop("media", []),
-            "metadata": data,  # remaining data goes into metadata
+            "metadata": (data.pop("metadata", {}) if isinstance(data.get("metadata"), dict) else {}) | data,  # pop metadata only if it's a dict
         }
+
 
     def get_document_from_dict(self, data: dict, source_file: str, id_in_file: int | str):
         """
