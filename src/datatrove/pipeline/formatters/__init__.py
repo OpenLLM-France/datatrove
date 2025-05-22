eecbba7f4403e290870904e4cd584d70017761a3
Oligou <oliviergouvert@gmail.com>
2025-05-22 10:06:27 +0200

    Add pii cleaning (phone numbers)

--- src/datatrove/pipeline/formatters/__init__.py
+++ src/datatrove/pipeline/formatters/__init__.py
@@ -1,3 +1,3 @@
 from .ftfy import FTFYFormatter
-from .pii import PIIFormatter
+from .pii import PIIFormatter, PhoneNumberPII, MorePIIFormatter
 from .symbol_lines_remover import SymbolLinesFormatter
