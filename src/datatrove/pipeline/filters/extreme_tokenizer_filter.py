f47e8d8228f1ea3098cd4e5e99f04d6417296ea2
Oligou <oliviergouvert@gmail.com>
2025-06-20 15:29:24 +0200

    improve extreme tokenizer filrer

--- src/datatrove/pipeline/filters/extreme_tokenizer_filter.py
+++ src/datatrove/pipeline/filters/extreme_tokenizer_filter.py
@@ -4,6 +4,7 @@ from datatrove.pipeline.filters.base_filter import BaseFilter
 from functools import cached_property
 import os
 from datatrove.utils.text import SPLIT_TEXT_DOCUMENTS, split_into_parts, split_into_chunks
+import re
 
 def load_tokenizer(name_or_path: str) -> "Tokenizer":
     from tokenizers import Tokenizer
@@ -26,7 +27,10 @@ class ExtremeTokenizerFilter(BaseFilter):
         min_token_per_char: float = 0.2,
         max_token_per_char: float = 0.38,
         filter_mode: str = SPLIT_TEXT_DOCUMENTS,
-        label_only = False,
+        replace_span: str = "",
+        removed_spans_in_metadata = False, # For debugging only
+        threshold_removal = 0.5,
+        label_only=False,
     ):
         super().__init__(exclusion_writer)
         self.tokenizer_name_or_path = tokenizer_name_or_path
@@ -37,6 +41,9 @@ class ExtremeTokenizerFilter(BaseFilter):
         self.max_token_per_char = max_token_per_char
         self.min_length_chunk = min_length_chunk # in number of characters
         self.filter_mode = filter_mode
+        self.replace_span = replace_span
+        self.removed_spans_in_metadata = removed_spans_in_metadata
+        self.threshold_removal = threshold_removal
 
     @cached_property
     def tokenizer(self) -> "Tokenizer":
@@ -46,26 +53,36 @@ class ExtremeTokenizerFilter(BaseFilter):
         return tokenizer
     
     def filter(self, doc: Document) -> bool | tuple[bool, str]:
+        doc.text = doc.text.strip()
         units = split_into_parts(doc.text, mode=self.filter_mode)
         encoded_chunks = self.tokenizer.encode_batch(units)
         token_lengths = [len(encoded_chunk.ids) for encoded_chunk in encoded_chunks]
 
         kept_spans = []
+        removed_spans = []
         doc.metadata["token_per_chars"] = []
         for unit, token_length in zip(units, token_lengths):
             if len(unit) == 0:
                 token_per_char = float('inf')
             else:
                 token_per_char = token_length / len(unit)
-            doc.metadata["token_per_chars"].append(token_per_char)
+            if self.removed_spans_in_metadata:
+                doc.metadata["token_per_chars"].append(token_per_char)
             if self.min_token_per_char < token_per_char < self.max_token_per_char:
                 kept_spans.append(unit)
                 self.stat_update("kept_span")
             if (token_per_char < self.min_token_per_char) or (token_per_char > self.max_token_per_char):
+                kept_spans.append("<<removed_span>>")
                 self.stat_update("removed_span")
-        clean_text = "".join(kept_spans)
-        if clean_text.strip() == "":
-            return False
+                removed_spans.append(unit)
+
+        clean_text = "\n".join(kept_spans)
+        if self.removed_spans_in_metadata:
+            doc.metadata['removed_spans'] = removed_spans
+        doc.metadata['num_removed_spans'] = len(removed_spans)
+        clean_text = re.sub(r'(<<removed_span>>\s*)+', self.replace_span, clean_text).strip()
+        if len(removed_spans)/len(units) >= self.threshold_removal:
+            return False, "too_much_removed_span"
         else:
             doc.text = clean_text
             return True
