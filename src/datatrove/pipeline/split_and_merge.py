9d4035de96e9d135efcd7815b8f9699f847599f4
Jeronymous <jeromelouradour@gmail.com>
2025-07-21 16:16:07 +0200

    Bugfix: add the last document. New feature: filtering when merging

--- src/datatrove/pipeline/split_and_merge.py
+++ src/datatrove/pipeline/split_and_merge.py
@@ -1,6 +1,8 @@
+import contextlib
 from datatrove.utils.text import split_into_parts
 from datatrove.pipeline.base import PipelineStep
 from datatrove.data import Document, DocumentsPipeline
+from datatrove.pipeline.writers.disk_base import DiskWriter
 import re
 
 class SplitDocument(PipelineStep):
@@ -51,34 +53,59 @@ class MergeDocument(PipelineStep):
     def __init__(
         self,
         replace_span: str = "\n\n[...]\n\n",
+        min_character_ratio: float = 0.5,
+        min_words = None,
+        exclusion_writer: DiskWriter = None,
     ):
         super().__init__()
         self.replace_span = replace_span
+        self.min_character_ratio = min_character_ratio
+        self.min_words = min_words
+        self.exclusion_writer = exclusion_writer
 
     def run(self, data: DocumentsPipeline, rank: int = 0, world_size: int = 1) -> DocumentsPipeline:
-        doc = None 
-        for chunk in data:
-            if (doc is None) or (doc.id != chunk.id):
-                # Yield the previous document if it exists
-                if doc is not None:
-                    if previous_chunk_index < doc.metadata["chunk_count"] - 1:
-                        doc.text += "<<removed_span>>"
-                    doc.text = re.sub(r'(<<removed_span>>)+', self.replace_span, doc.text).strip()
-                    doc.metadata.update(chunk_metadata)  # Merge chunk metadata
-                    doc.metadata["final_length"] = len(doc.text)
+        with self.exclusion_writer if self.exclusion_writer else contextlib.nullcontext() as writer:
+            doc = None
+            for chunk in data:
+                if (doc is None) or (doc.id != chunk.id):
+                    # Yield the previous document if it exists
+                    if doc is not None:
+                        if previous_chunk_index < doc.metadata["chunk_count"] - 1:
+                            doc.text += "<<removed_span>>"
+                        doc.text = re.sub(r'(<<removed_span>>)+', self.replace_span, doc.text).strip()
+                        doc.metadata.update(chunk_metadata)  # Merge chunk metadata
+                        doc.metadata["final_length"] = len(doc.text)
+                        if (not self.min_character_ratio or doc.metadata["final_length"] / doc.metadata["initial_length"] >= self.min_character_ratio) \
+                            and (not self.min_words or len(doc.text.split()) >= self.min_words):
+                            yield doc
+                        elif self.exclusion_writer:
+                            doc.metadata["filter_reason"] = f'kept characters: {doc.metadata["final_length"]/doc.metadata["initial_length"]} < {self.min_character_ratio} or (nb words={len(doc.text.split())} < {self.min_words})'
+                            writer.write(doc, rank)
+                    # New document, reset the doc and chunk_metadatas
+                    doc = Document(
+                        id=chunk.id, 
+                        text="",
+                        metadata={k.removeprefix("__doc__"): v for k, v in chunk.metadata.items() if k.startswith("__doc__")}
+                    )
+                    chunk_metadata = defaultdict(list)  # Initialize as dict of lists
+                    previous_chunk_index = -1
+                # Append the chunk text
+                if chunk.metadata["chunk_index"] != previous_chunk_index + 1:
+                    doc.text += "<<removed_span>>"
+                doc.text += chunk.text
+                # Merge chunk metadata into dict of lists
+                merge_chunk_metadata(chunk_metadata, chunk.metadata)
+                previous_chunk_index = chunk.metadata["chunk_index"]
+
+            if doc is not None:
+                if previous_chunk_index < doc.metadata["chunk_count"] - 1:
+                    doc.text += "<<removed_span>>"
+                doc.text = re.sub(r'(<<removed_span>>)+', self.replace_span, doc.text).strip()
+                doc.metadata.update(chunk_metadata)  # Merge chunk metadata
+                doc.metadata["final_length"] = len(doc.text)
+                if (not self.min_character_ratio or doc.metadata["final_length"] / doc.metadata["initial_length"] >= self.min_character_ratio) \
+                    and (not self.min_words or len(doc.text.split()) >= self.min_words):
                     yield doc
-                # New document, reset the doc and chunk_metadatas
-                doc = Document(
-                    id=chunk.id, 
-                    text="",
-                    metadata={k.removeprefix("__doc__"): v for k, v in chunk.metadata.items() if k.startswith("__doc__")}
-                )
-                chunk_metadata = defaultdict(list)  # Initialize as dict of lists
-                previous_chunk_index = -1
-            # Append the chunk text
-            if chunk.metadata["chunk_index"] != previous_chunk_index + 1:
-                doc.text += "<<removed_span>>"
-            doc.text += chunk.text
-            # Merge chunk metadata into dict of lists
-            merge_chunk_metadata(chunk_metadata, chunk.metadata)
-            previous_chunk_index = chunk.metadata["chunk_index"]
+                elif self.exclusion_writer:
+                    doc.metadata["filter_reason"] = f'kept characters: {doc.metadata["final_length"]/doc.metadata["initial_length"]} < {self.min_character_ratio} (or nb words={len(doc.text.split())} < {self.min_words})'
+                    writer.write(doc, rank)
